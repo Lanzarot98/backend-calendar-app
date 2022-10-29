38350c92141ab765a25b91cf94c7d3ed350986bf
lanzarot <luis-389@hotmail.com>
2022-10-29 12:28:20 -0500

    Express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,23 @@
 const {response} = require('express');
+const { validationResult } = require('express-validator');
 
 
 const createUser = (req, res = response) => {
     
     const { name, email, password } = req.body;
+    
 
-    if ( name.length < 5 ) {
+    // manejo de errores
+    const errors = validationResult( req );
+    if( !errors.isEmpty() ) {
         return res.status( 400 ).json({
             ok: false,
-            msg: 'The name must be more than 5 letters'
+            errors: errors.mapped()
         });
     }
-    
-    res.json({
+    // console.log(errors);
+
+    res.status( 201 ).json({
         ok: true,
         msg: 'register',
         name,
@@ -26,7 +31,16 @@ const loginUser = (req, res = response) => {
     
     const { email, password } = req.body;
 
-    res.json({
+    // manejo de errores
+    const errors = validationResult( req );
+    if( !errors.isEmpty() ) {
+        return res.status( 400 ).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+
+    res.status( 201 ).json({
         ok: true,
         msg: 'login',
         email,
--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,27 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 const { createUser, revalidateToken, loginUser } = require('../controllers/auth')
 
-router.post('/new', createUser ); 
+router.post(
+    '/new', 
+    [ // middlewares
+        check('name', 'The name is required').not().isEmpty(),
+        check('email', 'The email is required').isEmail(),
+        check('password', 'The password must be more than 5 letters').isLength({ min: 6 }),
+    ],
+    createUser ); 
 
-router.post('/', loginUser ); 
+router.post(
+    '/',
+    [
+        check('email', 'The email is required').isEmail(),
+        check('password', 'The password is required and is more than 5 letters').isLength({ min: 6 }),
+    ],
+    loginUser ); 
 
 router.get('/renew', revalidateToken ); 
 
