f0bf8f9752f3f2b683f95258875adbaca3b18e6c
lanzarot <luis-389@hotmail.com>
2022-11-01 21:20:39 -0500

    Create user un our dataBase

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,28 @@
 const {response} = require('express');
+const Usuario = require('../models/Usuario');
 
-const createUser = (req, res = response) => {
+const createUser = async(req, res = response) => {
     
-    const { name, email, password } = req.body;
+    // const { name, email, password } = req.body;
 
-    res.status( 201 ).json({
-        ok: true,
-        msg: 'register',
-        name,
-        email, 
-        password
-    })
+    try {
+        
+        const usuario = new Usuario( req.body );
+    
+        await usuario.save();
+    
+        res.status( 201 ).json({
+            ok: true,
+            msg: 'register',
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            ok: false,
+            msg: 'Please ask to the administrator'
+        });
+    }
 
 }
 
