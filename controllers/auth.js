88b7678a39e35b4112129bac036b813044f8da26
lanzarot <luis-389@hotmail.com>
2023-02-04 15:39:24 -0500

    details uid and name in auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -103,8 +103,8 @@ const revalidateToken = async (req, res = response) => {
 
     res.json({
         ok: true,
-        // uid,
-        // name,
+         uid,
+        name,
         token
     })
 
